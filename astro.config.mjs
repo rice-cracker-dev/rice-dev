32425550f5ab5b737d893a4b44ec271528a7711f
rice-cracker-dev <ricecracker2234@outlook.com>
2024-04-06 16:51:54 +0700

    added new blog added tag searching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
-
 import icon from 'astro-icon';
+
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), icon()],
+  integrations: [tailwind(), svelte(), icon(), mdx()],
   site: 'https://rice-dev.tech',
   markdown: {
     shikiConfig: {
--- src/content/posts/my-first-blog.md
+++ src/content/posts/my-first-blog.md
@@ -2,10 +2,11 @@
 title: 'my first blog'
 description: 'my first blog wow'
 published: true
-date: 2021-10-10
+date: 2024-04-09
 tags: ['blog', 'general', 'website']
 ---
 
 # welcome to my website
 
 this is where i will post my rants n shit.
+also probably some notes for my future projects.
